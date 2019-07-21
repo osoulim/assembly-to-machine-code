f839527379ba3568c431f539c8e179b5abefbcf4
Mohammadreza Osouli <mohammadreza.osouli@gmail.com>
2019-07-21 23:34:37 +0430

    Add one displacement

--- src/lib/asm2machine.js
+++ src/lib/asm2machine.js
@@ -2,71 +2,20 @@
 import parser from './parser';
 import { register_op_32bit, rm_16bit, rm_32bit, scale } from './codes';
 
-/* 
-mov al, bl
-{"tag":"Instruction","type":"Memory","name":"mov","operands":[
-    {"tag":"Reg","name":"al","size":8},
-    {"tag":"Reg","name":"bl","size":8}]}
+export function dec2hex_reverse(disp) {
+    let tmp = disp.toString(16);
+    if (tmp.length <= 2) {
+        return tmp.length == 2 ? tmp : "0" + tmp;
+    }
+    while (tmp.length <= 8)
+        tmp = "0" + tmp;
+    const zz = tmp.slice(0, 2),
+        yy = tmp.slice(2, 4),
+        xx = tmp.slice(4, 6),
+        ww = tmp.slice(6, 8);
+    return ww + xx + yy + zz;
+}
 
-mov ecx, eax
-{"tag":"Instruction","type":"Memory","name":"mov","operands":[
-    {"tag":"Reg","name":"ecx","size":32},
-    {"tag":"Reg","name":"eax","size":32}]}
-
-mov edx, [123]
-{"tag":"Instruction","type":"Memory","name":"mov","operands":[
-    {"tag":"Reg","name":"edx","size":32},
-    {"tag":"Cast","size":0,
-        "value":{"tag":"Number",
-            "value":123,"deref":true, "index":{"index":null,"constant":null}}}]}
-
-mov edi, [ebx]
-{"tag":"Instruction","type":"Memory","name":"mov","operands":[
-    {"tag":"Reg","name":"edi","size":32},
-    {"tag":"Cast","size":0,
-        "value":{"tag":"Mem",
-            "baseRegister":{"tag":"Reg","name":"ebx","size":32},"index":{"index":null,"constant":null}}}]}
-
-
-mov eax, [esi + 123]
-{"tag":"Instruction","type":"Memory","name":"mov","operands":[
-    {"tag":"Reg","name":"eax","size":32},
-    {"tag":"Cast","size":0,
-        "value":{"tag":"Mem",
-            "baseRegister":{"tag":"Reg","name":"esi","size":32},
-            "index":{"index":null,"constant":{"tag":"Number","value":123,"size":4}}}}]}
-
-mov eax, [123 + esi*4]
-{"tag":"Instruction","type":"Memory","name":"mov","operands":[
-    {"tag":"Reg","name":"eax","size":32},
-    {"tag":"Cast","size":0,
-        "value":{"tag":"Number","value":123,"deref":true,
-            "index":{
-                "index":{
-                    "register":{"tag":"Reg","name":"esi","size":32},
-                    "multiplier":4},"constant":null}}}]}
-
-mov eax, [ebx + esi*4]
-{"tag":"Instruction","type":"Memory","name":"mov","operands":[
-    {"tag":"Reg","name":"eax","size":32},
-    {"tag":"Cast","size":0,
-        "value":{"tag":"Mem",
-            "baseRegister":{"tag":"Reg","name":"ebx","size":32},
-            "index":{
-                "index":{
-                    "register":{"tag":"Reg","name":"esi","size":32},
-                    "multiplier":4},"constant":null}}}]}
-
-{"tag":"Instruction","type":"Memory","name":"mov","operands":[
-    {"tag":"Cast","size":0,
-        "value":{
-            "tag":"Mem",
-            "baseRegister":{"tag":"Reg","name":"eax","size":32},"index":{"index":null,"constant":null}}},
-    {"tag":"Cast","size":0,
-        "value":{
-            "tag":"Number",
-            "value":16,"size":4}}]}
-*/
 
 export function asm2machine(instruction) {
     let parsed_instruction = parser.parse(instruction);
@@ -95,14 +44,45 @@ export function asm2machine(instruction) {
         reg = result.D === "0" ? operand2 : operand1;
 
     if (rm_field.tag === "Reg") {
+        /*
+            mov al, bl
+            {"tag":"Instruction","type":"Memory","name":"mov","operands":[
+                {"tag":"Reg","name":"al","size":8},
+                {"tag":"Reg","name":"bl","size":8}]}
+
+            mov ecx, eax
+            {"tag":"Instruction","type":"Memory","name":"mov","operands":[
+                {"tag":"Reg","name":"ecx","size":32},
+                {"tag":"Reg","name":"eax","size":32}]}
+        */
         result.MOD = "11";
         result.Reg = register_op_32bit[reg.name];
         result.RM = register_op_32bit[rm_field.name];
-        // TODO 
     }
     else if (rm_field.value.tag === "Number") {
-        result.MOD = "00";
+        /*
+        mov edx, [123]
+        {"tag":"Instruction","type":"Memory","name":"mov","operands":[
+            {"tag":"Reg","name":"edx","size":32},
+            {"tag":"Cast","size":0,
+                "value":{"tag":"Number",
+                    "value":123,"deref":true, "index":{"index":null,"constant":null}}}]}
+
+        mov eax, [123 + esi*4]
+        {"tag":"Instruction","type":"Memory","name":"mov","operands":[
+            {"tag":"Reg","name":"eax","size":32},
+            {"tag":"Cast","size":0,
+                "value":{"tag":"Number","value":123,"deref":true,
+                    "index":{
+                        "index":{
+                            "register":{"tag":"Reg","name":"esi","size":32},
+                            "multiplier":4},"constant":null}}}]}
+
+        */
+        result.MOD = rm_field.value.index.index == null ? "00" : "10";
         result.RM = rm_field.value.index.index == null ? "101" : "100";
+        result.Reg = register_op_32bit[reg.name];
+        result.Displacement = dec2hex_reverse(rm_field.value.value)
         // TODO 
     }
     else {
@@ -118,3 +98,44 @@ export function asm2machine(instruction) {
     console.log(result);
 }
 
+/*
+
+
+mov edi, [ebx]
+{"tag":"Instruction","type":"Memory","name":"mov","operands":[
+    {"tag":"Reg","name":"edi","size":32},
+    {"tag":"Cast","size":0,
+        "value":{"tag":"Mem",
+            "baseRegister":{"tag":"Reg","name":"ebx","size":32},"index":{"index":null,"constant":null}}}]}
+
+
+mov eax, [esi + 123]
+{"tag":"Instruction","type":"Memory","name":"mov","operands":[
+    {"tag":"Reg","name":"eax","size":32},
+    {"tag":"Cast","size":0,
+        "value":{"tag":"Mem",
+            "baseRegister":{"tag":"Reg","name":"esi","size":32},
+            "index":{"index":null,"constant":{"tag":"Number","value":123,"size":4}}}}]}
+
+
+mov eax, [ebx + esi*4]
+{"tag":"Instruction","type":"Memory","name":"mov","operands":[
+    {"tag":"Reg","name":"eax","size":32},
+    {"tag":"Cast","size":0,
+        "value":{"tag":"Mem",
+            "baseRegister":{"tag":"Reg","name":"ebx","size":32},
+            "index":{
+                "index":{
+                    "register":{"tag":"Reg","name":"esi","size":32},
+                    "multiplier":4},"constant":null}}}]}
+
+{"tag":"Instruction","type":"Memory","name":"mov","operands":[
+    {"tag":"Cast","size":0,
+        "value":{
+            "tag":"Mem",
+            "baseRegister":{"tag":"Reg","name":"eax","size":32},"index":{"index":null,"constant":null}}},
+    {"tag":"Cast","size":0,
+        "value":{
+            "tag":"Number",
+            "value":16,"size":4}}]}
+*/
